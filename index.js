ba9f103b8dac438774b557269186fd54637b381e
webHikari <q5503212@gmail.com>
2024-09-25 22:37:18 +0300

    add error handling, impove prompt and decrease length needed to check message

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const { BOT_TOKEN, MISTRAL_API_KEY } = process.env;
 const client = new Mistral({ apiKey: MISTRAL_API_KEY });
 
 const instruction = `
-Ты - классификатор спама в чате Winderton. Твоя задача - определить, является ли сообщение спамом или нет, учитывая, что сообщение может содержать шутки, мемы или другой развлекательный контент, который не является спамом.
+Ты - классификатор спама в чате Winderton. Твоя задача - определить, является ли сообщение спамом или нет, учитывая, что сообщение может содержать шутки, мемы или другой развлекательный контент, который не является спамом, этот чат - чат программистов, основной спам который тут есть это предложения о заработке, пожалуйста классифицируй только по параметру является ли сообщение предложением о заработке/наборе людей куда-то или нет.
+СПАМ-сообщение так же может содержать большое количество ссылок например @GOLD_SIGNALS 
 
 Внимательно прочитай сообщение и ответь, используя следующий формат:
 
@@ -20,36 +21,40 @@ const instruction = `
 `;
 
 async function checkMessageByAI(message) {
-    const prompt = instruction + "\n" + message;
+    try {
+        const prompt = instruction + "\n" + message;
 
-    const chatResponse = await client.chat.complete({
-        model: "mistral-large-latest",
-        messages: [
-            {
-                role: "user",
-                content:
-                    prompt +
-                    "Send response in JSON and only JSON format. WITHOUT MARKDOWN FORMATTING. Avoid backticks. Only JSON. If u give me response like that ```json response``` i will kill myself right fucking now",
-            },
-        ],
-        response_format: { type: "json_object" },
-    });
+        const chatResponse = await client.chat.complete({
+            model: "mistral-large-latest",
+            messages: [
+                {
+                    role: "user",
+                    content:
+                        prompt +
+                        "Send response in JSON and only JSON format. WITHOUT MARKDOWN FORMATTING. Avoid backticks. Only JSON. If u give me response like that ```json response``` i will kill myself right fucking now",
+                },
+            ],
+            response_format: { type: "json_object" },
+        });
 
-    let response = chatResponse?.choices[0]?.message.content;
-    if (response.startsWith("```json") && response.endsWith("```")) {
-        response = response.slice(7, -3).trim();
+        let response = chatResponse?.choices[0]?.message.content;
+        if (response.startsWith("```json") && response.endsWith("```")) {
+            response = response.slice(7, -3).trim();
+        }
+        response = JSON.parse(response);
+        console.log(response);
+        return response.is_spam;
+    } catch (error) {
+        console.error(error)
     }
-    response = JSON.parse(response);
-    console.log(response);
-    return response.is_spam;
 }
 
 const bot = new Bot(BOT_TOKEN);
 
 bot.on("message", async (ctx) => {
     let isSpam = false;
-    if (typeof ctx.message.text == "string" && ctx.message.text.length > 80) {
-        isSpam = await checkMessageByAI(ctx.message.text);
+    if (typeof ctx.message.text == "string" && ctx.message.text.length > 50) {
+        isSpam = await checkMessageByAI(ctx.message.text) || false;
     }
     if (isSpam) {
         await ctx.reply("spam", {
@@ -57,6 +62,20 @@ bot.on("message", async (ctx) => {
         });
     }
     console.log(`${isSpam}: ${ctx.from.id}:\n${ctx.message.text}`);
+});
+
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}`);
+
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error(`Error in request: ${e.description}`);
+    } else if (e instanceof HttpError) {
+        console.error(`Could not connect to Telegram: ${e}`);
+    } else {
+        console.error(`Unknown error: ${e}`);
+    }
 });
 
 bot.start();
